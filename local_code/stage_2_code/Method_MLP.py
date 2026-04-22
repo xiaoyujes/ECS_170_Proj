a3a14683f7cc675da824aa3fcaebbd59c56de383
Pieride <cxzh@ucdavis.edu>
2026-04-21 21:13:04 -0700

    full batch, 4 hidden layers, max 97.6% acc
    Coldplay

--- local_code/stage_2_code/Method_MLP.py
+++ local_code/stage_2_code/Method_MLP.py
@@ -15,32 +15,34 @@ import numpy as np
 class Method_MLP(method, nn.Module):
     data = None
     # it defines the max rounds to train the model
-    max_epoch = 200
+    max_epoch = 300
     # it defines the learning rate for gradient descent based optimizer for model learning
-    learning_rate = 5e-4
+    learning_rate = 1e-2
 
-    # it defines the MLP model architecture, e.g.,
+    # it defines the the MLP model architecture, e.g.,
     # how many layers, size of variables in each layer, activation function, etc.
     # the size of the input/output portal of the model architecture should be consistent with our data input and desired output
     def __init__(self, mName, mDescription):
+        self.history = []
         method.__init__(self, mName, mDescription)
         nn.Module.__init__(self)
         # check here for nn.Linear doc: https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
-        self.fc_layer_1 = nn.Linear(784, 512)
         # check here for nn.ReLU doc: https://pytorch.org/docs/stable/generated/torch.nn.ReLU.html
+        self.fc_layer_1 = nn.Linear(784, 512)
         self.activation_func_1 = nn.ReLU()
+
         self.fc_layer_2 = nn.Linear(512, 256)
-        # check here for nn.Softmax doc: https://pytorch.org/docs/stable/generated/torch.nn.Softmax.html
         self.activation_func_2 = nn.ReLU()
+
         self.fc_layer_3 = nn.Linear(256, 128)
         self.activation_func_3 = nn.ReLU()
-        self.fc_layer_4 = nn.Linear(128, 10)
-        self.activation_func_4 = nn.Softmax(dim=1)
 
-        self.dropout = nn.Dropout(p=0.025)
+        self.fc_layer_4 = nn.Linear(128, 64)
+        self.activation_func_4 = nn.ReLU()
 
-        self.historical_loss = []
-        self.historical_accuracy = []
+        self.fc_layer_5 = nn.Linear(64, 10)
+        # check here for nn.Softmax doc: https://pytorch.org/docs/stable/generated/torch.nn.Softmax.html
+        # self.activation_func_3 = nn.Softmax(dim=1)
 
     # it defines the forward propagation function for input x
     # this function will calculate the output layer by layer
@@ -48,33 +50,27 @@ class Method_MLP(method, nn.Module):
     def forward(self, x):
         '''Forward propagation'''
         # hidden layer embeddings
-        h = self.activation_func_1(self.fc_layer_1(x))
-        # outout layer result
+        h1 = self.activation_func_1(self.fc_layer_1(x))
+        h2 = self.activation_func_2(self.fc_layer_2(h1))
+        h3 = self.activation_func_3(self.fc_layer_3(h2))
+        h4 = self.activation_func_4(self.fc_layer_4(h3))
+        y_pred = self.fc_layer_5(h4)  # raw logits for 10 classes
+        return y_pred
+        # output layer result
         # self.fc_layer_2(h) will be a nx2 tensor
         # n (denotes the input instance number): 0th dimension; 2 (denotes the class number): 1st dimension
         # we do softmax along dim=1 to get the normalized classification probability distributions for each instance
-        h = self.dropout(h)
-        h = self.activation_func_2(self.fc_layer_2(h))
-        h = self.dropout(h)
-        h = self.activation_func_3(self.fc_layer_3(h))
-        h = self.dropout(h)
-        y_pred = self.fc_layer_4(h)
-        return y_pred
 
     # backward error propagation will be implemented by pytorch automatically
     # so we don't need to define the error backpropagation function here
 
     def train(self, X, y):
         # check here for the torch.optim doc: https://pytorch.org/docs/stable/optim.html
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        optimizer = torch.optim.Adagrad(self.parameters(), lr=self.learning_rate)
         # check here for the nn.CrossEntropyLoss doc: https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
         loss_function = nn.CrossEntropyLoss()
         # for training accuracy investigation purpose
         accuracy_evaluator = Evaluate_Accuracy('training evaluator', '')
-
-        best_loss = float('inf')
-        patience = 20
-        patience_counter = 0
 
         # it will be an iterative gradient updating process
         # we don't do mini-batch, we use the whole input as one batch
@@ -96,37 +92,47 @@ class Method_MLP(method, nn.Module):
             # update the variables according to the optimizer and the gradients calculated by the above loss.backward function
             optimizer.step()
 
-            #storing loss for the loss convergence curve
-            self.historical_loss.append(train_loss.item())
-
-            if train_loss.item() < best_loss:
-                best_loss = train_loss.item()
-                patience_counter = 0
-            else:
-                patience_counter += 1
-                if patience_counter >= patience:
-                    print(f'Early stopping at epoch {epoch}')
-                    break
-
-            if epoch%10 == 0: #originally epoch%100 but did epoch%1 just to get an idea of what is going on (change back when have stable model version)
+            if epoch % 10 == 0:
                 accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
-                train_accuracy = accuracy_evaluator.evaluate()
-                #storing accuracy for roc curve / auc
-                self.historical_accuracy.append(train_accuracy)
-                print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
-    
+                metrics = accuracy_evaluator.evaluate()
+
+                print('Epoch:', epoch, 'Accuracy:', metrics['accuracy'], 'Loss:', train_loss.item())
+                print('Weighted - Precision:', metrics['precision_weighted'],
+                      'Recall:', metrics['recall_weighted'],
+                      'F1:', metrics['f1_weighted'])
+                print('Macro - Precision:', metrics['precision_macro'],
+                      'Recall:', metrics['recall_macro'],
+                      'F1:', metrics['f1_macro'])
+                print('Micro - Precision:', metrics['precision_micro'],
+                      'Recall:', metrics['recall_micro'],
+                      'F1:', metrics['f1_micro'])
+
+                self.history.append({
+                    'epoch': epoch,
+                    'loss': train_loss.item(),
+                    'accuracy': metrics['accuracy'],
+                    'precision_weighted': metrics['precision_weighted'],
+                    'recall_weighted': metrics['recall_weighted'],
+                    'f1_weighted': metrics['f1_weighted'],
+                    'precision_macro': metrics['precision_macro'],
+                    'recall_macro': metrics['recall_macro'],
+                    'f1_macro': metrics['f1_macro'],
+                    'precision_micro': metrics['precision_micro'],
+                    'recall_micro': metrics['recall_micro'],
+                    'f1_micro': metrics['f1_micro']
+                })
+
     def test(self, X):
         # do the testing, and result the result
         y_pred = self.forward(torch.FloatTensor(np.array(X)))
         # convert the probability distributions to the corresponding labels
         # instances will get the labels corresponding to the largest probability
         return y_pred.max(1)[1]
-    
+
     def run(self):
         print('method running...')
         print('--start training...')
         self.train(self.data['train']['X'], self.data['train']['y'])
         print('--start testing...')
         pred_y = self.test(self.data['test']['X'])
-        return {'pred_y': pred_y, 'true_y': self.data['test']['y']}
-            
+        return {'pred_y': pred_y, 'true_y': self.data['test']['y'],'history': self.history}
