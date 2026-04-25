02def6505d6d723292c957c463be5d03c617d7c7
aryac2004 <abchaudhari@ucdavis.edu>
2026-04-24 17:21:05 -0700

    Add files via upload

---
+++ local_code/stage_2_code/Arya/Dataset_Loader.py
@@ -0,0 +1,38 @@
+#Arya
+from local_code.base_class.dataset import dataset
+
+
+class Dataset_Loader(dataset):
+    data = None
+    dataset_source_folder_path = None
+    dataset_source_train_file_name = None
+    dataset_source_test_file_name = None
+
+    def __init__(self, dName=None, dDescription=None):
+        super().__init__(dName, dDescription)
+
+    def load(self):
+        print('loading training data...')
+        X_train, y_train = [], []
+
+        f = open(self.dataset_source_folder_path + self.dataset_source_train_file_name, 'r')
+        for line in f:
+            elements = [int(i) for i in line.strip().split(',')]
+            X_train.append([i / 255.0 for i in elements[1:]])
+            y_train.append(elements[0])
+        f.close()
+
+        print('loading testing data...')
+        X_test, y_test = [], []
+
+        f = open(self.dataset_source_folder_path + self.dataset_source_test_file_name, 'r')
+        for line in f:
+            elements = [int(i) for i in line.strip().split(',')]
+            X_test.append([i / 255.0 for i in elements[1:]])
+            y_test.append(elements[0])
+        f.close()
+
+        return {
+            'X_train': X_train, 'y_train': y_train,
+            'X_test': X_test, 'y_test': y_test
+        }
